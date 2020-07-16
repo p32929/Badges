c06d7d0fdc86be6f68b8fad0ccd06f2f008b026e
p32929 <p32929@yahoo.com>
2020-07-16 20:20:03 +0600

    Updates

---
+++ precache-manifest.3d8383e7e524fa2ba110f14643453cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b292b662622e3ab355812e2f2cc6ab45",
+    "url": "/Badges/index.html"
+  },
+  {
+    "revision": "82d80e066ef0dcdcf998",
+    "url": "/Badges/static/css/main.1c3fdcaf.chunk.css"
+  },
+  {
+    "revision": "8ac6333c241cd4b0756e",
+    "url": "/Badges/static/js/2.c76ade54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Badges/static/js/2.c76ade54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d80e066ef0dcdcf998",
+    "url": "/Badges/static/js/main.4e5dedc9.chunk.js"
+  },
+  {
+    "revision": "6b8807d2182587b82385",
+    "url": "/Badges/static/js/runtime-main.b10b1726.js"
+  }
+]);
